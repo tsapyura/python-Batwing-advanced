14805cd65eb4a27b3a7242f995005a57ad910af1
tsapyura <58184759+tsapyura@users.noreply.github.com>
2022-05-18 15:39:50 +0300

    Update second.py
    commit 4

--- cursor_git/second.py
+++ cursor_git/second.py
@@ -1,37 +1,4 @@
-
-
-<<<<<<< HEAD
-print("hello")
-print("tsap")
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-
 print("second BRABCH")
-
-
->>>>>>> second
-
 print("Cursor")
-
-
-<<<<<<< HEAD
->>>>>>> first
-=======
-
->>>>>>> second
-=======
 print("Yura")
-=======
-
-
-
-
 print("first branch")
->>>>>>> first
-
->>>>>>> first
