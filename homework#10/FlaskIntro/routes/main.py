997e833686e594596c9c669412cf8183dab41451
Yura Tsap <tsapyura1999@gmail.com>
2022-06-15 20:33:13 +0300

    fixing mistakes

--- homework#10/FlaskIntro/routes/main.py
+++ homework#10/FlaskIntro/routes/main.py
@@ -5,9 +5,7 @@ from flask import render_template
 def hello_world2():
     return render_template("index.html")
 
-@app.route("/add/<first_number>/<second_number>")
+@app.route("/add/<int:first_number>/<int:second_number>")
 def adding_2_numbera(first_number, second_number):
-    a = [first_number,second_number]
-    b = list(map(int, a))
-    s = str(sum(b))
+    s = first_number + second_number
     return render_template("Adding.html", variable=s)
