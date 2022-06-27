405e2215d70bb52740f6b233c709a22ce1c50479
Yura Tsap <tsapyura1999@gmail.com>
2022-06-27 23:14:23 +0300

    Adding homework #12 Flask&Rest

--- homework#12/main.py
+++ homework#12/main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-# from user_api import user_router
+from user_api import user_router
 from book_api import book_router
 
 app = Flask(__name__)
-# app.register_blueprint(user_router)
+app.register_blueprint(user_router)
 app.register_blueprint(book_router)
 
 status_code_failed = 404
